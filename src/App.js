aee9f2003698ca56ff3bc72cf263de6a83f6fb76
benos <benantonn@gmail.com>
2023-07-28 12:39:38 -0500

    set up router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Projects from './components/Projects';
+import Resume from './components/Resume';
 function App() {
   return (
-    <div className="App"></div>
+    <div className='App'> 
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/resume' element={<Resume />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
