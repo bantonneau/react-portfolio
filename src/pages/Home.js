c9b749581b0a69da020310e0130829367f237454
benos <benantonn@gmail.com>
2023-07-31 18:22:36 -0500

    rework styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,20 +16,24 @@ function Home() {
           <div class='image-cropper'>
             <img src={benos} class="rounded" />
           </div>
-          <h2>Hi, My Name is Ben</h2>
+          <div className='herotext'>
+            <h2>Hi there! I'm Ben</h2>
+            <p>Full Stack Web Developer</p>
+          </div>
         </div>
+      </div>
+      <div className='aboutme'>
         <div className='prompt'>
           <p>
-            I like to code xd xd
-            {/* I'm a Full Stack Developer with a strong understanding of both front-end and back-end technologies. My previous experience in graphic and apparel design gives me a unique set of skills and an eye for beauty and good design. When I'm not building websites you will find me lifting weights, designing clothes, or working on unity games with my friends. */}
+            I'm a Full Stack Developer with a strong understanding of both front-end and back-end technologies. My previous experience in graphic and apparel design gives me a unique set of skills and an eye for beauty and good design. When I'm not building websites you will find me lifting weights, designing clothes, or working on unity games with my friends.
           </p>
+        </div>
+        <div className='socialLinks'>
           <LinkedIn />
           <Email />
           <GitHub className='githubIcon' />
         </div>
       </div>
-      {/*Make an array of technical skills and soft skills and map them to tile components */}
-      {/*Add toggle to switch between technical and soft skills, with nice tile fade in animation when switching? */}
       <div className='skills'>
 
         <div className="toggleContainer">
