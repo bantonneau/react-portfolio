3c377aebdc8cb23842d5f8b731cbdd06f05d3764
benos <benantonn@gmail.com>
2023-08-01 14:45:31 -0500

    fix issues with custom domain

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import ProjectDisplay from './pages/ProjectDisplay'
 function App() {
   return (
     <div className='App'> 
-      <Router basename="/react-portfolio">
+      <Router>
        <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
