9d8e0fe1fe0f58f6f278cddb1e6f5faee2e00beb
benos <benantonn@gmail.com>
2023-08-02 12:49:11 -0500

    embed resume pdf and style

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import '../styles/resume.css'
 
 function Resume() {
   return (
-    <div>Resume</div>
+    <div className='pdf'>
+      <object width="100%" height="400" data="https://www.docdroid.net/0RV7iHg/web-dev-bennett-antonneau-resume-3-pdf" type="application/pdf">   </object>
+    </div>
   )
 }
 
