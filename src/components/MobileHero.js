9d7ff64adeefbcf6e3400fca06a87abeadc1c79e
benos <benantonn@gmail.com>
2023-08-08 12:47:48 -0500

    fix mobile/responsive styling

--- src/components/MobileHero.js
+++ src/components/MobileHero.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
 import benos from '../assets/laptop.jpg'
+import ContactMe from './ContactMe'
+
 
 
 function MobileHero() {
+    const [isOpen, setIsOpen] = useState(false)
     return (
         <section className='hero'>
             <div className='image-cropper'>
@@ -12,6 +16,13 @@ function MobileHero() {
             <div className='herotext'>
                 <p>I'm a Full Stack Developer with a strong understanding of both front-end and back-end technologies. My previous experience in graphic and apparel design gives me a unique set of skills with the Adobe Creative Suite. When I'm not building websites you will find me lifting weights, designing clothes, or working on unity games with my friends.</p>
             </div>
+            <section className='buttons'>
+                <button onClick={() => setIsOpen(true)}>Contact Me</button>
+                <ContactMe className='modal'>Modal Content</ContactMe>
+                <Link to="/resume"> 
+                Resume
+                </Link>
+            </section>
         </section>
     )
 }
